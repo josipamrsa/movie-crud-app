5c29abc4c79f91c01fe201910c00a267d67f1600
Josipa <josipamrsa@outlook.com>
2025-01-26 12:31:40 +0100

    minor updates

---
+++ app/src/main/java/com/jmrsa/moviecrudapp/presentation/fragments/home/HomeContract.kt
@@ -0,0 +1,4 @@
+package com.jmrsa.moviecrudapp.presentation.fragments.home
+
+class HomeContract {
+}
---
+++ app/src/main/java/com/jmrsa/moviecrudapp/presentation/fragments/search/SearchViewModel.kt
@@ -0,0 +1,4 @@
+package com.jmrsa.moviecrudapp.presentation.fragments.search
+
+class SearchViewModel {
+}
